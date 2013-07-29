fee18a18e2d3e164e1177d63ab98497be13e4001
sam66s <sam66s@gmail.com>
2013-07-29 21:07:03 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buf = new Buffer (16);
+buf = fs.readFileSync(index.html);
+buf.toString('utf-8');
+
+
+
 app.get('/', function(request, response) {
   response.send('Hello World 3!');
 });
