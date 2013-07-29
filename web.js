6318ef17bbe4d7782c834e0bb78dfdad33671c45
sam66s <sam66s@gmail.com>
2013-07-29 23:23:45 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,14 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var htmlfile = "index.html";
 
-//var buf = new Buffer (16);
-//buf = fs.readFileSync(index.html);
-//output_sam = buf.toString('utf-8');
-output_sam = 'helloworld';
+
+var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-  response.send(output_sam);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
 var port = process.env.PORT || 5000;
