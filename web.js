f8e36ec2bbed84190bf8b4e70b958c7e0d7a22f4
sam66s <sam66s@gmail.com>
2013-07-29 21:27:44 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,12 +4,12 @@ app.use(express.logger());
 
 var buf = new Buffer (16);
 buf = fs.readFileSync(index.html);
-//buf.toString('utf-8');
+output_sam = buf.toString('utf-8');
 
 
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'));
+  response.send(output_sam);
 });
 
 var port = process.env.PORT || 5000;
